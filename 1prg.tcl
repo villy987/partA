7f50a824a899da12f44cff15e9fc9a60230058bd
villy987 <122398120+villy987@users.noreply.github.com>
2023-02-10 22:43:57 +0530

    Update 1prg.tcl

--- 1prg.tcl
+++ 1prg.tcl
@@ -43,3 +43,16 @@ proc finish {} {
 $ns run
 
 
+1.awk:
+BEGIN {
+tcp_count=0;
+udo_count=0;
+}{
+if($1 == "d" && $5 == "tcp")
+tcp_count++;
+if($1 == "d" && $5 == "cbr")
+udp_count++;
+} END {
+printf("TCP %d\n",tcp_count);
+printf("UDP %d\n",udp_count);
+}
